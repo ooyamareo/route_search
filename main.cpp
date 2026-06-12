ed07eec540e554ef25546ac35ba42a113b47f293
大山怜央 <reo.ooyama28@gmail.com>
2026-06-12 10:33:52 +0900

    read_connection_csv完成

--- main.cpp
+++ main.cpp
@@ -19,17 +19,22 @@ std::vector<std::string>  index_to_line; //上のmapの逆写像
 
 
 class Edge{
+    
     int to_index;
     int line_index;
     int time;
     int fare;
+    public:
+
+
     Edge(int to_index,int line_index,int time,int fare):to_index(to_index),line_index(line_index),time(time),fare(fare) {}
 };
 
 
+
 std::vector<std::vector<Edge>> graph;
 
-
+/*
 void read_station_csv(){
     std::string str_buf;
     std::ifstream ifs_station("~/route/station.csv");
@@ -67,6 +72,7 @@ void read_station_csv(){
 
 }
 
+//*/
 
 void read_connection_csv(){
     std::string str_buf;
@@ -94,30 +100,50 @@ void read_connection_csv(){
         //str_bufをコンマ区切りで読む
         
 
-        int current_staiton_index,next_station_index,line_index,time,fare;
+        int current_station_index,next_station_index,line_index,time,fare;
 
 
-        std::vector<int*> address={&current_staiton_index,&next_station_index,&line_index,&time,&fare};
+        std::vector<int*> address={&current_station_index,&next_station_index,&line_index,&time,&fare};
         for(int i = 0; i < 5 ; i++){
             getline(str_buf_stream,str_comma_buf,',');
-            if (i <= 1){
-                std::string station=str_comma_buf;
 
+
+            // iの値で分岐
+            if (i <= 1){
+                
+                
+                //初めて見る駅なら番号を付ける
                 if(!station_to_index.count(str_comma_buf)){
                     station_to_index[str_comma_buf]=index_to_station.size();
                     index_to_station.push_back(str_comma_buf);
                 }
 
-
+                //出発駅と到着駅のインデックスを保存
                 *address[i]=station_to_index[str_comma_buf];
             }
             else if( i == 2){
-                //std::string station=str_comma_buf;
+                
+                //初めて見る路線なら番号を付ける
+                if(!line_to_index.count(str_comma_buf)){
+                    line_to_index[str_comma_buf]=index_to_line.size();
+                    index_to_line.push_back(str_comma_buf);
+                }
+                //路線のインデックスを保存
+                *address[i]=line_to_index[str_comma_buf];
+
             }
             else{
 
+                //運賃，または経由時間を保存
+                *address[i]=std::stoi(str_comma_buf);
+
             }
         }
+        graph.resize(index_to_station.size());
+        
+        graph[current_station_index].push_back(Edge{next_station_index,line_index,time,fare});
+
+        graph[next_station_index].push_back(Edge{current_station_index,line_index,time,fare});
        
 
 
@@ -132,12 +158,34 @@ void read_connection_csv(){
     return;
 }
 
+
+void dijkstra(){
+
+}
+
+
+
+
 int main(){
 
     
-    read_station_csv();
+    //read_station_csv();
 
     read_connection_csv();
+
+    while(true){
+
+        std::string cmd;
+        std::cout <<"from>";
+        if (!getline(std::cin, cmd)) {
+            break; // EOF
+        }
+
+        if (cmd == "exit") {
+            break;
+        }
+
+    }
     return 0; 
     
     
